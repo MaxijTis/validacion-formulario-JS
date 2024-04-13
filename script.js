69ccd9150d6263ef1bbd3c9f3be83e81d715a265
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-13 02:39:49 -0300

    validando contrasena y email

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById('formulario').addEventListener('submit'),(event) => {
+document.getElementById('formulario').addEventListener('submit',(event) => {
     event.preventDefault()
 
     //Validar nombre
     let entradaNombre = document.getElementById('name')
     let errorNombre = document.getElementById('nameError')
 
-    if(entradaNombre.ariaValueMax.trim() === ''){
+    if(entradaNombre.value.trim() === ''){
         errorNombre.textContent = 'Por favor, introducí tu nombre'
         errorNombre.classList.add('error-message')
     }else{
@@ -14,13 +14,34 @@ document.getElementById('formulario').addEventListener('submit'),(event) => {
     }
 
     //validar correo
+    let emailEntrada = document.getElementById('email')
+    let emailError = document.getElementById('emailError')
+    let emailPattern = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/; //patron de validacion basico
+    if (!emailPattern.test(emailEntrada.value)) {
+        emailError.textContent = 'Por favor, introducí un mail válido'
+        emailError.classList.add('error-message')
+    }else{
+        emailError.textContent = ''
+        emailError.classList.remove('error-message')
+    }
 
     //validar contraseña
+    let contrasenaEntrada = document.getElementById('password')
+    let contrasenaError = document.getElementById('passwordError')
+    if (contrasenaEntrada.value.length < 8) {
+            contrasenaError.textContent = 'Por favor, introducí una contraseña válida'
+            contrasenaError.classList.add('error-message')
+    }else{
+        contrasenaError.textContent = ''
+        contrasenaError.classList.remove('error-message')
+    }
 
     //si todos los campos son validos enviar formulario
 
-
-
-
-
+if (!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent) {
+    //Backend que reciba la info
+    alert('El formuario se ha enviado con exito.')
+    document.getElementById('formulario').reset();
 }
+
+})
