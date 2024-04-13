2b54433e938581fc90186981b0712eeb6a3d3d6e
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-13 02:53:41 -0300

    validaciones finales de contraseña y mail

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById('formulario').addEventListener('submit',(event) => {
+document.getElementById('formulario').addEventListener('submit', (event) => {
     event.preventDefault()
 
     //Validar nombre
     let entradaNombre = document.getElementById('name')
     let errorNombre = document.getElementById('nameError')
 
-    if(entradaNombre.value.trim() === ''){
+    if (entradaNombre.value.trim() === '') {
         errorNombre.textContent = 'Por favor, introducí tu nombre'
         errorNombre.classList.add('error-message')
-    }else{
+    } else {
         errorNombre.textContent = ''
         errorNombre.classList.remove('error-message')
     }
@@ -20,7 +20,7 @@ document.getElementById('formulario').addEventListener('submit',(event) => {
     if (!emailPattern.test(emailEntrada.value)) {
         emailError.textContent = 'Por favor, introducí un mail válido'
         emailError.classList.add('error-message')
-    }else{
+    } else {
         emailError.textContent = ''
         emailError.classList.remove('error-message')
     }
@@ -28,20 +28,20 @@ document.getElementById('formulario').addEventListener('submit',(event) => {
     //validar contraseña
     let contrasenaEntrada = document.getElementById('password')
     let contrasenaError = document.getElementById('passwordError')
-    if (contrasenaEntrada.value.length < 8) {
-            contrasenaError.textContent = 'Por favor, introducí una contraseña válida'
-            contrasenaError.classList.add('error-message')
-    }else{
+    let contrasenaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
+    if (!contrasenaPattern.test(contrasenaEntrada.value)) {
+        contrasenaError.textContent = 'Por favor, introducí una contraseña válida. Debe tener mas de 8 caracteres, mayusculas, minusculas, numeros y caracteres especiales'
+        contrasenaError.classList.add('error-message')
+    } else {
         contrasenaError.textContent = ''
         contrasenaError.classList.remove('error-message')
     }
 
     //si todos los campos son validos enviar formulario
 
-if (!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent) {
-    //Backend que reciba la info
-    alert('El formuario se ha enviado con exito.')
-    document.getElementById('formulario').reset();
-}
-
+    if (!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent) {
+        //Backend que reciba la info
+        alert('El formuario se ha enviado con exito.')
+        document.getElementById('formulario').reset();
+    }
 })
