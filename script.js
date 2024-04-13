921997a488b02bdf67eb13d0d4f18931b24ee725
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-13 01:54:18 -0300

    Iniciamos el proyecto de formulario

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('formulario').addEventListener('submit'),(event) => {
+    event.preventDefault()
+
+    //Validar nombre
+
+    //validar correo
+
+    //validar contraseña
+
+    //si todos los campos son validos enviar formulario
+
+
+
+
+
+}
